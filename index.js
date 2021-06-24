399bc63dd75108a28d459b9c7029d9003b4ac2d6
ItzaMi <rros00@gmail.com>
2021-06-24 13:12:25 +0100

    [Setup] Move databaseID to .env file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Client } = require("@notionhq/client");
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
 module.exports = async function getDatabase() {
-  const databaseId = "601f5547fa004969b151c26806af80c2";
+  const databaseId = process.env.NOTION_API_DATABASE;
   const response = await notion.databases.query({ database_id: databaseId });
 
   const responseResults = response.results.map((page) => {
