302a0cdf43b24d5b3824313a63e2c8b4f4e729e6
ItzaMi <rros00@gmail.com>
2021-07-05 08:41:23 +0100

    [server] import `getDatabase` and `newEntryToDatabase` differently and add a `post` to submit the new user form

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require("express");
-const getDatabase = require("./index");
+const moduleToFetch = require("./index");
+const getDatabase = moduleToFetch.getDatabase;
+const newEntryToDatabase = moduleToFetch.newEntryToDatabase;
 const port = 8000;
 
 const app = express();
 
 app.use(express.static("public"));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.get("/users", async (req, res) => {
   const users = await getDatabase();
   res.json(users);
+});
+
+app.post("/submit-form", (req, res) => {
+  const name = req.body.name;
+  const role = req.body.role;
+  newEntryToDatabase(name, role);
+  res.redirect("/");
+  res.end();
 });
 
 app.listen(port, console.log(`Server started on ${port}`));
